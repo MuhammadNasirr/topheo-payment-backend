53b75188c9bb22e4fb619e4230145c3abe79ba7e
Hameez <syed_hameez21@outlook.com>
2021-05-31 01:05:59 +0500

    auth open

--- src/middlewares/AuthMiddleware.js
+++ src/middlewares/AuthMiddleware.js
@@ -1,21 +1,22 @@
 import { firebase } from "../utils/firebase";
 
 function authMiddleware(request, response, next) {
-  const headerToken = request.headers.authorization;
-  if (!headerToken) {
-    return response.status(401).send({ message: "No token provided" });
-  }
+  next();
+  // const headerToken = request.headers.authorization;
+  // if (!headerToken) {
+  //   return response.status(401).send({ message: "No token provided" });
+  // }
 
-  if (headerToken && headerToken.split(" ")[0] !== "Bearer") {
-    response.status(401).send({ message: "Invalid token" }).status(401);
-  }
+  // if (headerToken && headerToken.split(" ")[0] !== "Bearer") {
+  //   response.status(401).send({ message: "Invalid token" }).status(401);
+  // }
 
-  const token = headerToken.split(" ")[1];
-  firebase
-    .auth()
-    .verifyIdToken(token)
-    .then(() => next())
-    .catch(() => response.status(403).send({ message: "Could not authorize" }));
+  // const token = headerToken.split(" ")[1];
+  // firebase
+  //   .auth()
+  //   .verifyIdToken(token)
+  //   .then(() => next())
+  //   .catch(() => response.status(403).send({ message: "Could not authorize" }));
 }
 
 export default authMiddleware;
